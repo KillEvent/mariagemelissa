98c7ecafb0552cfb23d16d8d688e007fa2212c31
KillEvent <145292444+KillEvent@users.noreply.github.com>
2023-09-18 09:16:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const correctCode = "1234"; // Vous pouvez changer ce code
+const dog = document.getElementById('dog');
+const door = document.getElementById('door');
+const growlSound = document.getElementById('growlSound');
 
-function checkCode() {
-    const userInput = document.getElementById('codeInput').value;
+let hasPetTheDog = false;
 
-    if (userInput === correctCode) {
-        document.getElementById('result').textContent = "Bravo! Vous avez trouvé le code et vous êtes échappé!";
-        document.getElementById('result').style.color = "green";
-    } else {
-        document.getElementById('result').textContent = "Code incorrect. Essayez encore!";
-        document.getElementById('result').style.color = "red";
+dog.addEventListener('mouseover', function() {
+    if (!hasPetTheDog) {
+        growlSound.play();
     }
-}
+});
 
+dog.addEventListener('click', function() {
+    hasPetTheDog = true;
+    alert("Vous avez caressé le chien. Il semble plus content maintenant.");
+    dog.src = "chemin_vers_votre_image_de_chien_content.jpg"; // Changez l'image du chien pour montrer qu'il est content
+});
+
+door.addEventListener('click', function() {
+    if (hasPetTheDog) {
+        alert("Bravo! Vous avez su qu'il fallait caresser le chien pour entrer dans la maison.");
+    } else {
+        alert("Le chien ne semble pas content. Peut-être devriez-vous essayer de le caresser d'abord.");
+    }
+});
