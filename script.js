86e279ec1404187bf095467bebe920d89444d871
KillEvent <145292444+KillEvent@users.noreply.github.com>
2023-09-19 15:11:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const gameType = document.getElementById("gameType");
     const animator = document.getElementById("animator");
+    const distanceField = document.getElementById("distanceField");
+    const distance = document.getElementById("distance");
     const costumeBox = document.getElementById("costumeBox");
     const playerExtension = document.getElementById("playerExtension");
     const totalPrice = document.getElementById("totalPrice");
     const calculateButton = document.getElementById("calculateButton");
 
+    animator.addEventListener("change", function() {
+        if (animator.value === "250") {
+            distanceField.style.display = "block";
+        } else {
+            distanceField.style.display = "none";
+        }
+    });
+
     calculateButton.addEventListener("click", function() {
         let total = parseFloat(gameType.value) + parseFloat(animator.value);
+
+        if (animator.value === "250") {
+            total += parseFloat(distance.value) * 0.50;
+        }
 
         if (costumeBox.checked) {
             total += parseFloat(costumeBox.value);
