773500b9f5a4796cebbf15ca0fc57441846b46be
KillEvent <145292444+KillEvent@users.noreply.github.com>
2023-09-19 14:52:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    const gameType = document.getElementById("gameType");
+    const animator = document.getElementById("animator");
+    const costumeBox = document.getElementById("costumeBox");
+    const playerExtension = document.getElementById("playerExtension");
+    const totalPrice = document.getElementById("totalPrice");
+    const calculateButton = document.getElementById("calculateButton");
+
+    calculateButton.addEventListener("click", function() {
+        let total = parseFloat(gameType.value) + parseFloat(animator.value);
+
+        if (costumeBox.checked) {
+            total += parseFloat(costumeBox.value);
+        }
+
+        if (playerExtension.checked) {
+            total += parseFloat(playerExtension.value);
+        }
+
+        totalPrice.textContent = total.toFixed(2) + "€";
+    });
+});
